3b65894232299e5472632bd5ac50bf27d9a47a63
Marek S <mareks@live.com>
2024-04-09 21:22:07 +0300

    updates

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -3,20 +3,23 @@ import mongoose, { Mongoose } from "mongoose";
 const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-  con: Mongoose | null;
+  conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
 let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { con: null, promise: null };
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connectToDatabase = async () => {
-  if (cached.con) return cached.con;
+  if (cached.conn) return cached.conn;
 
-  if (!MONGODB_URL) throw new Error("MONGODB_URL is not set");
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
   cached.promise =
     cached.promise ||
@@ -25,7 +28,7 @@ export const connectToDatabase = async () => {
       bufferCommands: false,
     });
 
-  cached.con = await cached.promise;
+  cached.conn = await cached.promise;
 
-  return cached.con;
+  return cached.conn;
 };
