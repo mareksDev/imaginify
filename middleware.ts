fd245bb2739959aa0a21c26fe6fdac4c11519c1c
Marek S <mareks@live.com>
2024-04-13 20:33:41 +0300

    fix auth

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  publicRoutes: ["/", "/api/webhooks/clerk"],
+  publicRoutes: ["/", "/api/webhooks/clerk", "/api/webhooks/stripe"],
 });
 
 export const config = {
